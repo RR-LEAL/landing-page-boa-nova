95b2bc67427ff61275b23ff455f63620cb7c1d40
Rodrigo Rodrigues <rr.leal4@gmail.com>
2026-01-27 19:40:51 -0300

    refactor: remove unused Next.js configuration options for cleaner setup

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
-  basePath: '/landing-page-boa-nova',
-  images: {
-    unoptimized: true
-  },
+ 
 };
 
 export default nextConfig;
